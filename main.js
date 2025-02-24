4d232d1c47381cd22edcf70a8264cbd5c6537c0a
kundup <kundup61@hotmail.com>
2025-02-24 14:06:51 -0800

    background is defined, drawn

--- main.js
+++ main.js
@@ -1,23 +1,33 @@
 var canvas = document.getElementById("tetricity");
-var ctx = canvas.getContent("2d");
+var ctx = canvas.getContext("2d");
+const color = {backgroundcolor : "black"};
 
-function draw (){
-    drawbackground();
+function drawboard(){
+
+    ctx.fillStyle = color.backgroundcolor;
+    ctx.fillRect(0,0,canvas.width,canvas.height);
+}
+
+function drawshapes() {};
+
+function drawEveryting(){
     drawboard();
     drawshapes();
-};
+}
 
-function update(){    
-};
+function update() {};
 
 function gameloop(){
-    draw();
-    update();
-};
+    drawEveryting();
+    requestAnimationFrame(gameloop);
+}
+
+gameloop();
+
 
 
 
 
 // todo creat general concept of the game (canvas id, getcontext, etc) - done
 // todo create draft for game- first drawing shapes, board and background then updating the shapes and board and countiously drawing them - done
-// completing drawing functions
+// completing drawing board with black background - done
