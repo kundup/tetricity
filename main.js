1a7e6d2856cbd188446b54e1a37aa2cf886e1aef
kundup <kundup61@hotmail.com>
2025-02-24 13:21:26 -0800

    draft of the game determined

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
-// todo creat general concept of the game (canvas id, getcontext, and some functions)
+var canvas = document.getElementById("tetricity");
+var ctx = canvas.getContent("2d");
+
+function draw (){
+    drawbackground();
+    drawboard();
+    drawshapes();
+};
+
+function update(){    
+};
+
+function gameloop(){
+    draw();
+    update();
+};
+
+
+
+
+// todo creat general concept of the game (canvas id, getcontext, etc) - done
+// todo create draft for game- first drawing shapes, board and background then updating the shapes and board and countiously drawing them
