847b364a449eb197a7e25351298dfd1367f7299c
kundup <kundup61@hotmail.com>
2025-02-24 18:12:20 -0800

    shape object defined

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 var canvas = document.getElementById("tetricity");
 var ctx = canvas.getContext("2d");
 const color = {backgroundcolor : "black"};
+const shape = {L : [[1,0],[1,0],[1,1]], J : [[0,1],[0,1],[1,1]],
+I : [[1],[1],[1],[1]], O : [[1,1],[1,1]], S : [[0,1,1],[1,1,0]], Z : [[1,1,0],[0,1,1]], T : [[1,1,1],[0,1,0]]};
 
 function drawboard(){
 
