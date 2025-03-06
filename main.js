e9ced727099fc4438dd1651ae9f26e3f3ca9e6a7
kundup <kundup61@hotmail.com>
2025-03-05 19:48:05 -0800

    tilesize defined instead of tileheight and tilelenght

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 
 const canvas = document.getElementById("tetricity");
 const ctx = canvas.getContext("2d");
-
+const tilesize = 15;
+const velshapeY = tilesize * 0.1;
 const row = 30;
 const col = 30;
 const framegap = 1;
@@ -11,11 +12,7 @@ const shape = {L : [[1,0],[1,0],[1,1]], J : [[0,1],[0,1],[1,1]],
 I : [[1],[1],[1],[1]], O : [[1,1],[1,1]], S : [[0,1,1],[1,1,0]], Z : [[1,1,0],[0,1,1]], T : [[1,1,1],[0,1,0]], TT : [[0,1,0],[1,1,1]]};
 const Keylists = Object.keys(shape);
 let ranshape = Keylists.at(-1); // ** last element in the array
-
-const tilelenght = 15;
-const tileheight = 15;
-const shapeDown = 40;
-let shapeX = canvas.width / 2 - tilelenght
+let shapeX = canvas.width / 2 - tilesize
 let shapeY = 0;
 
 
@@ -28,11 +25,11 @@ function drawboard(){
         for (let j = 0; j < col; j++){
             if (grid [i][j] === 1) {
                 ctx.fillStyle = color.shapecolor;
-                ctx.fillRect (j * tilelenght, i * tileheight, tilelenght, tileheight)
+                ctx.fillRect (j * tilesize, i * tilesize, tilesize, tilesize)
 
-                ctx.strokeStyle = color.frame; // Siyah çerçeve
+                ctx.strokeStyle = color.frame;
                 ctx.lineWidth = framegap;
-                ctx.strokeRect(j * tilelenght, i * tileheight, tilelenght, tileheight);
+                ctx.strokeRect(j * tilesize, i * tilesize, tilesize, tilesize);
             }
         }        
 
@@ -45,19 +42,20 @@ function drawshapes() {
         for (let j = 0; j < shape[ranshape][i].length; j++) {
             if (shape[ranshape][i][j] == 1) { 
                 ctx.fillStyle = color.shapecolor;               
-                ctx.fillRect(shapeX + j * tilelenght, shapeY + i * tileheight, tilelenght, tileheight);
+                ctx.fillRect(shapeX + j * tilesize, shapeY + i * tilesize, tilesize, tilesize);
                 
                 ctx.strokeStyle = color.frame;
                 ctx.lineWidth = framegap;
-                ctx.strokeRect(shapeX + j * tilelenght, shapeY + i * tileheight, tilelenght, tileheight);
+                ctx.strokeRect(shapeX + j * tilesize, shapeY + i * tilesize, tilesize, tilesize);
             }
         }
     }
 };
 
-function collisonDetection () {
+function collisondetection (){
 
-} 
+}
+
 
 function Placetheshape (x, y){
 
@@ -71,12 +69,12 @@ function Placetheshape (x, y){
 }
 
 function moveshapes(){
-    shapeY += 1;
-    if (shapeY>= canvas.height- shape[ranshape].length * tileheight){
-        Placetheshape(shapeX/tilelenght, shapeY/tileheight)
+    shapeY += velshapeY;
+    if (shapeY >= canvas.height- shape[ranshape].length * tilesize){
+        Placetheshape(shapeX/tilesize, shapeY/tilesize)
         shapeY = 0;
         ranshape = Keylists[Math.floor(Math.random() * Keylists.length)];
-        shapeX = canvas.width / 2 - tilelenght;
+        shapeX = canvas.width / 2 - tilesize;
         
     } 
 }
@@ -89,14 +87,14 @@ document.addEventListener("keydown", function(event){
         }
     } else if (event.key === "ArrowRight"){
         shapeX += 15;
-        let shapeWidth = shape[ranshape][0]. length * tilelenght   
+        let shapeWidth = shape[ranshape][0]. length * tilesize   
         if (shapeX + shapeWidth > canvas.width - shapeWidth) {  
             shapeX = canvas.width - shapeWidth;
         }       
     } else if (event.key === "ArrowDown"){
-        shapeY += shapeDown; 
-                
-    }
+
+        shapeY += tilesize * 1.5;
+    }   
 });
 
 function drawEveryting(){
@@ -136,9 +134,10 @@ gameloop();
 // points mechanism
  
 
-// for the long term improvement add visual affects
+// for the long term improvement add visual affects and organise the game frame
 // for the long term improvement add possibility
-
+// for the long term improvment add next shape visuals
+// publish the game
 
 
 // ** represents : new js function usage
